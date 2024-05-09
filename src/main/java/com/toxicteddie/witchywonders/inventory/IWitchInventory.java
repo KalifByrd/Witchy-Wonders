8444a2e330979f81f1a0d33d12f7832f20fd6614
KalifByrd <89876335+KalifByrd@users.noreply.github.com>
2024-05-09 05:49:13 -0700

    Delete src/main directory
    updating

--- src/main/java/com/toxicteddie/witchywonders/inventory/IWitchInventory.java
+++
@@ -1,8 +0,0 @@
-package com.toxicteddie.witchywonders.inventory;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface IWitchInventory {
-    void setAdditionalSlot(int index, ItemStack stack);
-    ItemStack getAdditionalSlot(int index);
-}
