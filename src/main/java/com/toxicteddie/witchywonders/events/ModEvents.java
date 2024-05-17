9495215e84ee9c6010ced9ccc1c04871837ea961
KalifByrd <89876335+KalifByrd@users.noreply.github.com>
2024-05-17 01:15:34 -0700

    Delete src/main/java/com/toxicteddie/witchywonders directory
    updating

--- src/main/java/com/toxicteddie/witchywonders/events/ModEvents.java
+++
@@ -1,9 +0,0 @@
-package com.toxicteddie.witchywonders.events;
-
-import net.minecraftforge.fml.common.Mod;
-import com.toxicteddie.witchywonders.WitchyWonders;;
-
-@Mod.EventBusSubscriber(modid = WitchyWonders.MODID)
-public class ModEvents {
-    
-}
