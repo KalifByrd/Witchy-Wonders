088ea80f1693e95f0574ae10ee0d29f5dd8528a9
KalifByrd <89876335+KalifByrd@users.noreply.github.com>
2024-05-17 01:18:04 -0700

    java part 2

---
+++ src/main/java/com/toxicteddie/witchywonders/entity/client/ModModelLayers.java
@@ -0,0 +1,11 @@
+package com.toxicteddie.witchywonders.entity.client;
+
+import com.toxicteddie.witchywonders.WitchyWonders;
+
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.ResourceLocation;
+
+public class ModModelLayers {
+    public static final ModelLayerLocation MANDRAKE_LAYER = new ModelLayerLocation(
+        new ResourceLocation(WitchyWonders.MODID, "mandrake_layer"), "main");
+}
